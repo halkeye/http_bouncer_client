ad46d722d5c0cb5dd95cacd9b728a02aa7af9725
Gavin Mogan <gavin@kodekoan.com>
2014-06-14 11:54:41 -0700

    initial version of the http_bouncer_client

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+  'socket_server': 'http://localhost:3000/',
+  'channels': {
+    'eti/gavin': {
+      'server': 'http://localhost/dev_app'
+    }
+  }
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,65 @@
+var HttpProxyAgent = require('http-proxy-agent');
+//var HttpsProxyAgent = require('https-proxy-agent');
+var io = require('socket.io-client');
+var request = require('request');
+var url = require('url');
+var path = require('path');
+
+/* TODO: Figure out a nice solution to have config.local */
+var config = require('./config.js');
+
+var agent;
+if (process.env.http_proxy) {
+  agent = HttpProxyAgent.new(process.env.http_proxy);
+}
+
+var socket = io.connect(config.socket_server, { agent: agent });
+
+socket.on('connect_error', function() { console.log('connect_error', arguments); });
+socket.on('error', function() { console.log('error', arguments); });
+socket.on('event', function(data){ console.log('event', data); });
+socket.on('connect', function() { console.log('connected', arguments); });
+
+socket.on('request_channels', function() {
+  console.log('requesting channels');
+  Object.keys(config.channels).forEach(function(channel) {
+    socket.emit('listen_channel', channel);
+  });
+});
+
+socket.on('channel_data', function(channel, data) {
+  data = JSON.parse(data);
+  delete data.headers.host; // Public host != private host
+
+  var channel_config = config.channels[channel];
+
+  if (!channel_config) {
+    //console.log('channel_data', channel, data);
+    console.log('Unable to handle ', channel);
+    return;
+  }
+
+  /* Use data from socket server to build a nice url */
+  var parsed_url = url.parse(channel_config.server);
+  parsed_url.pathname = path.join(parsed_url.pathname, data.path);
+  if (data.query) {
+    delete parsed_url.search;
+    parsed_url.query = data.query;
+  }
+  parsed_url = url.format(parsed_url);
+
+  /* POST/GET that sucka */
+  request({
+    url: parsed_url, 
+    method: data.method,
+    headers: data.headers,
+    body: data.body,
+  }, function(error, response, body) {
+    if (error || response.statusCode !== 200) {
+      console.log('Unable to connect to target:', body);
+    } else {
+      console.log('Submitted to ' + parsed_url);
+    }
+  });
+
+});
